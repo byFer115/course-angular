61de8b8e2d8d546349db98645002e812e8c4509e
Fernando Hernandez <fernando.hernandez@solsersistem.net>
2022-09-26 18:42:27 -0500

    First commit

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+})
+export class EmpleadoComponent implements OnInit {
+  nombre="Luis";
+  apellido="Hernandez";
+  edad=18;
+  //empresa="Solser";
+  habilitacionDelCuadro= true;
+  usuRegistrado = false;
+  textoDeRegistro= "Usuario no registrado";
+
+  getRegistroUsuario(){
+    this.usuRegistrado = false;
+  }
+
+  setUsuarioRegistrado(){
+    alert ("El usuario se acaba de registrar");
+  }
+
+  setRegistroUsuario(event:Event){
+    if((<HTMLInputElement>event.target).value=="si"){
+      this.textoDeRegistro= "El usuario se acaba registrar";
+    }else{
+      this.textoDeRegistro= "No hay nadie registrado";
+    }
+
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
